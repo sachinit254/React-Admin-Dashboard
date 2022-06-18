6d660402e0ec746216d6156ac46b2210cf2fc6a7
sachinit254 <sachinit254@gmail.com>
2022-06-18 14:19:16 +0530

    navbar layout implementation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./home.scss";
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
-      <div className="homeContainer">container</div>
+      <div className="homeContainer">
+        <Navbar />
+        home container
+      </div>
     </div>
   );
 };
